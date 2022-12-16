3be1cc11413c84eab1f5c48177e4c9f6e29023e2
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-12-16 12:00:17 +0100

    the solution with classbasedviews added

--- todo/urls.py
+++ todo/urls.py
@@ -2,12 +2,23 @@
 from django.urls import path,include
 from .views import (
     home,
+    #fbv
     todo_list_created,
-    todo_get_del_upd
+    todo_get_del_upd,
+
+    #cbv
+    Todos,
+    TodosDetails
     )
 
 urlpatterns = [
     path('', home),
+    
+    #fbv urls
     path('todos/',todo_list_created),
-    path('todos/<int:pk>/', todo_get_del_upd)
+    path('todos/<int:pk>/', todo_get_del_upd),
+    
+    #cbv
+    path('todos_cbv/', Todos.as_view()),
+    path('todos_cbv/<int:pk>', TodosDetails.as_view()) #classbaseviews 'de <int:pk> pk olarak yazilmali. Id olarak yazarsam hata verir
 ]
